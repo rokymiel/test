e01c46888712bdf1d765ef3dfc80fdd48e09034f
rokymiel <rokymiel@gmail.com>
2020-09-23 15:17:36 +0300

    AA@@

--- UISeminar/ViewController.swift
+++ UISeminar/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // hsgfjhfgshjfgjhsgjhfjsd
+        //fhsgf sdjfsdjhdshjf
         //cicrileView.layer.cornerRadius=cicrileView.bounds.height/2
         // Do any additional setup after loading the view.
     }
