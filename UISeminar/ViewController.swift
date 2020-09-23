ef2efd4544725c8951b0e382b404fa1dc0379e1c
rokymiel <rokymiel@gmail.com>
2020-09-23 15:05:02 +0300

    begin

---
+++ UISeminar/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  UISeminar
+//
+//  Created by Михаил on 17.09.2020.
+//  Copyright © 2020 Tinkoff. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var cicrileView: UIView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //cicrileView.layer.cornerRadius=cicrileView.bounds.height/2
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
